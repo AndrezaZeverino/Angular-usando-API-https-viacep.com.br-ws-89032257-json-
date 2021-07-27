d617e6168519c409235b3601a625436c63351a3f
Andreza Zeverino <82824036+AndrezaZeverino@users.noreply.github.com>
2021-07-27 16:07:38 -0300

    Add files via upload

---
+++ app/modelo/endereco.ts
@@ -0,0 +1,8 @@
+export class Endereco {
+// Para pegar as informações 
+    cep:string = "";
+    logradouro:string = "";
+    localidade:string = "";
+    bairro:string = "";
+    uf:string ="";
+}
